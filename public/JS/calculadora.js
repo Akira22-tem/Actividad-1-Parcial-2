9f35289f7ab614961dd7543bcc79ea29bf3eba2f
Kevin Yugla <kiyugla@espe.edu.ec>
2025-06-25 11:35:22 -0500

    creacion de secciones del historial y los resultados en el componente web

--- public/JS/calculadora.js
+++ public/JS/calculadora.js
@@ -59,6 +59,23 @@ class CalculadoraBasica extends HTMLElement {
                 </button>
               </div>
             </div>
+            <!-- seccion de los resultados -->
+            <div class="mb-4">
+              <div class="alert alert-dark border-danger bg-black text-white">
+                <h5 class="text-danger fw-bold mb-2">📊 RESULTADO:</h5>
+                <div id="areaResultado" class="fs-4 fw-bold text-center py-3 text-secondary">
+                  Esperando operación...
+                </div>
+              </div>
+              <!-- Seccion del historial -->
+              <div class="card bg-black border-danger">
+              <div class="card-header bg-danger text-white d-flex justify-content-between align-items-center">
+                <h5 class="mb-0 fw-bold">📋 HISTORIAL</h5>
+                <button type="button" class="btn btn-outline-light btn-sm fw-bold" id="botonLimpiarHistorial">
+                  🧹 LIMPIAR
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
