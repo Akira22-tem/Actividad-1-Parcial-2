891a63abd8d81009845489f098e5c7f89baecbbc
Kevin Yugla <kiyugla@espe.edu.ec>
2025-06-25 14:04:43 -0500

    se agrego el listener para evento cálculo-completado y extraer datos

--- public/JS/Eventos.js
+++ public/JS/Eventos.js
@@ -1,1 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+      // evento para el calculo completado
+  document.addEventListener('calculo-completado', function (evento) {
+    const contenedor = document.getElementById('eventos');
+    const {
+      primerNumero,
+      segundoNumero,
+      tipoOperacion,
+      simboloOperacion,
+      resultadoOperacion,
+      marcaTiempo,
+    } = evento.detail;
+
+});
